70ddfdbf5fca401f43a835af825c7d41308fdca0
saulabreu31 <53130862+saulabreu31@users.noreply.github.com>
2024-11-16 13:23:45 -0500

    Saul Commit

--- app.py
+++ app.py
@@ -1,24 +1,96 @@
-from flask import Flask
-from flask import Flask, render_template, request, jsonify
-import files
-
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Flask-Login setup
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Dummy user database
+users = {}
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(user_id)
+
+# Forms
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        InputRequired(), EqualTo('password', message='Passwords must match')
+    ])
+    submit = SubmitField('Register')
 
 @app.route('/')
+@login_required
 def home():
-    return render_template('index.html', title='Study Flow')
+    return render_template('index.html', username=current_user.username)
 
-@app.route('/uploadForm', methods=['GET', 'POST'])
-def uploadFiles():
-    return render_template("uploadFile.html", title = "Upload Files Form")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        for user in users.values():
+            if user.username == form.username.data and check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('home'))
+        flash('Invalid username or password')
+    return render_template('login.html', form=form)
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return {'error': 'No file part'}, 400
-    file = request.files['file']
-    return files.handle_file_upload(file)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = generate_password_hash(form.password.data, method='sha256')
+        if username not in [user.username for user in users.values()]:
+            user_id = str(len(users) + 1)
+            users[user_id] = User(user_id, username, password)
+            flash('Registration successful! Please log in.')
+            return redirect(url_for('login'))
+        else:
+            flash('Username already exists.')
+    return render_template('register.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/files')
+@login_required
+def files():
+    return render_template('files.html')
+
+@app.route('/calendar')
+@login_required
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/notes')
+@login_required
+def notes():
+    return render_template('notes.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
