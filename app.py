e7905f1c406d1158cc16be49280818d3fa56eee3
MacMelendez <102198069+MacMelendez@users.noreply.github.com>
2024-11-15 22:36:52 -0500

    Added File Management 1st Version

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import files
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='Home Page')
+    return render_template('index.html', title='Study Flow')
+
+@app.route('/uploadForm', methods=['GET', 'POST'])
+def uploadFiles():
+    return render_template("uploadFile.html", title = "Upload Files Form")
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return {'error': 'No file part'}, 400
+    file = request.files['file']
+    return files.handle_file_upload(file)
 
 if __name__ == '__main__':
     app.run(debug=True)
